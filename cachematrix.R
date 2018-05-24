e9ab5425f33b2e30ceb6680a0afc98409c75cb07
MementoMakoMori <becca.a.holley@gmail.com>
2018-05-24 18:25:33 -0400

    saved cachematrix with my code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+##JHU/Coursera R Programming
+##Programming Assignment 2
 
-## Write a short comment describing this function
+## set values and functions for caching solved matrices
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInv <- function(solve) inv <<- solve(x)
+  getInv <- function() inv
+  list(set = set,
+       get = get,
+       setInv = setInv,
+       getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## Function to return cached values, or solve
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInv()
+  if(!is.null(inv)){
+      message("getting cached data")
+      return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data,...)
+  x$setInv(inv)
+  inv
 }
